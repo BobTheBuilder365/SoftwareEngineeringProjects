c08d2aad1d4a2f0133f31730de6d3136bedcd7ca
Matthias Wyss <Matthias Wyss@LAPTOP-HP-MW>
2020-07-14 12:47:48 +0200

    update ListView Example

---
+++ src/topic06_IO_Lists_Tables_FileExercise/FileExercise.java
@@ -0,0 +1,28 @@
+package topic06_IO_Lists_Tables_FileExercise;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FileExercise extends Application {
+
+	private FileExercise_View view;
+	private FileExercise_Controller controller;
+	private FileExercise_Model model;
+	
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void init() {
+		// Initialization - but note that model does not yet exist!
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		this.model = new FileExercise_Model();
+		this.view = new FileExercise_View(stage, model);
+		this.controller = new FileExercise_Controller(view, model);
+		view.start();
+	}
+}
---
+++ src/topic06_IO_Lists_Tables_FileExercise/FileExercise_View.java
@@ -0,0 +1,39 @@
+package topic06_IO_Lists_Tables_FileExercise;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FileExercise_View {
+	private final FileExercise_Model model;
+	private final Stage stage;
+	
+	// GUI controls
+	Button btnRead = new Button("Read file 'numbers.txt'");
+	Label lblNumbers = new Label();
+	Label lblTotal = new Label();
+
+	public FileExercise_View(Stage stage, FileExercise_Model model) {
+		this.stage = stage;
+		this.model = model;
+		
+		
+		VBox root = new VBox(btnRead, lblNumbers, lblTotal);
+		root.getStyleClass().add("vbox");
+		
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		scene.getStylesheets().add(
+				getClass().getResource("FileExercise.css").toExternalForm());
+		stage.setTitle("File exercise");
+	}
+	
+	public void start() {
+		stage.show();
+	}
+}

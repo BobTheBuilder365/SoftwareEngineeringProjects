49e5a0f859b0058a566dbf315346848700a0d44b
BobTheBuider365 <BobTheBuilder365@users.noreply.github.com>
2020-07-15 09:49:49 +0200

    reconfig GIT - Both laptops now with GITHUB user

---
+++ src/topic01_ButtonClick_MVC/GiTTest.java
@@ -0,0 +1,5 @@
+package topic01_ButtonClick_MVC;
+
+public class GiTTest {
+
+}
--- src/topic06_IO_Lists_Tables/tableViewExample/TestGit.java
+++
@@ -1,7 +0,0 @@
-package topic06_IO_Lists_Tables.tableViewExample;
-
-public class TestGit {
-	
-	String zeichen = "String";
-
-}
